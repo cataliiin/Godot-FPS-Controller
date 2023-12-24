9a20411dd928190c08788f7a81771dcdb6016f75
cataliiin <75584656+cataliiin@users.noreply.github.com>
2023-12-24 20:00:11 +0200

    Add files via upload

--- player/player_camera.gd
+++ player/player_camera.gd
@@ -2,14 +2,34 @@ extends Node3D
 
 @export_node_path("Node3D") var head_nodepath
 @export_node_path("CharacterBody3D") var player_nodepath
-var mouse_sensitivity = 0.03
-var mouse_smoothness = 20
+var mouse_sensitivity
+var mouse_smoothness
 
 var camera_input : Vector2
 var rotation_velocity : Vector2
 
 @onready var head = get_node(head_nodepath)
 @onready var player = get_node(player_nodepath)
+
+func _ready():
+	# MOUSE
+	mouse_sensitivity = player.mouse_sensitivity
+	mouse_smoothness = remap(player.mouse_sensitivity, 0,10,0, 1.0)
+	
+	# HEAD BOOBING
+	hb_lerp_speed = player.hb_lerp_speed
+	hb_running_speed = player.hb_running_speed
+	hb_walking_speed = player.hb_walking_speed
+	hb_crouching_speed = player.hb_crouching_speed
+	
+	hb_running_intensity = player.hb_running_intensity
+	hb_walking_intensity = player.hb_walking_intensity
+	hb_crouching_intensity = player.hb_crouching_intensity
+	
+	# DYNAMIC FOV
+	fov_lerp_speed = player.fov_lerp_speed
+	default_fov = player.default_fov
+	running_fov = player.running_fov
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -20,38 +40,39 @@ func _input(event):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func _process(delta):
-	rotation_velocity = rotation_velocity.lerp(camera_input * mouse_sensitivity, delta * mouse_smoothness)
-	player.rotate_y(deg_to_rad(-rotation_velocity.x))
-	head.rotate_x(deg_to_rad(-rotation_velocity.y))
-	head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
-	camera_input = Vector2.ZERO
+	
+	if event is InputEventMouseMotion:
+		player.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
 func _physics_process(delta):
-	head_boobing(delta)
+	if player.headbob:
+		head_boobing(delta)
+	if player.dymanic_fov:
+		dynamic_fov(delta)
 
 # HEAD BOOBING
 
-const hb_lerp_speed = 10.0
+var hb_lerp_speed = 10.0
 
-const hb_running_speed = 18.0
-const hb_walking_speed = 14.0
-const hb_crouching_speed = 10.0
+var hb_running_speed = 18.0 
+var hb_walking_speed = 14.0
+var hb_crouching_speed = 10.0
 
-const hb_running_intensity = 0.12
-const hb_walking_intensity = 0.05
-const hb_crouching_intensity = 0.05
+var hb_running_intensity = 0.12
+var hb_walking_intensity = 0.05
+var hb_crouching_intensity = 0.05
 
-var hb_vector = Vector2.ZERO
+var hb_vector = Vector3.ZERO
 var hb_index = 0.0
 var hb_current_intensity = 0.0
 
 @onready var player_eyes = $".."
 
 func head_boobing(delta):
-	
-	match player.player_state:
+
+	match player.state:
 		player.PlayerState.running:
 			hb_current_intensity = hb_running_intensity
 			hb_index += hb_running_speed * delta
@@ -63,11 +84,33 @@ func head_boobing(delta):
 			hb_index += hb_crouching_speed * delta
 	
 	if player.is_on_floor() and player.direction != Vector3.ZERO:
-		hb_vector.y = sin(hb_index)
 		hb_vector.x = sin(hb_index/2)+0.5
+		hb_vector.y = sin(hb_index)
+		hb_vector.z = sin(hb_index/2)*0.1
 		
-		player_eyes.position.y = lerp(player_eyes.position.y, hb_vector.y * (hb_current_intensity/2), delta * hb_lerp_speed)
 		player_eyes.position.x = lerp(player_eyes.position.x, hb_vector.x * hb_current_intensity, delta * hb_lerp_speed)
+		player_eyes.position.y = lerp(player_eyes.position.y, hb_vector.y * (hb_current_intensity/2), delta * hb_lerp_speed)
+		player_eyes.rotation.z = lerp(player_eyes.rotation.z, hb_vector.z * hb_current_intensity, delta * hb_lerp_speed)
 	else:
-		player_eyes.position.y = lerp(player_eyes.position.y,0.0 , delta * hb_lerp_speed)
 		player_eyes.position.x = lerp(player_eyes.position.x,0.0 , delta * hb_lerp_speed)
+		player_eyes.position.y = lerp(player_eyes.position.y,0.0 , delta * hb_lerp_speed)
+		player_eyes.rotation.z = lerp(player_eyes.rotation.z, 0.0, delta * hb_lerp_speed)
+
+# DYNAMIC FOV
+
+var fov_lerp_speed
+
+var default_fov
+var running_fov
+
+func dynamic_fov(delta):
+	if player.state == player.PlayerState.running and player.direction != Vector3.ZERO and self.fov != running_fov:
+		self.fov = lerp(self.fov, running_fov, delta * fov_lerp_speed)
+	elif self.fov != default_fov:
+		self.fov = lerp(self.fov, default_fov, delta * fov_lerp_speed)
+
+
+
+
+
+
